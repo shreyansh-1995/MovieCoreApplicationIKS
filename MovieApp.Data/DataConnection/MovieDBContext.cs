e2847fde5c4b3adf03781aa8572fecbf26824499
Shreyansh <shreyansh.jan29@gmail.com>
2022-06-03 18:33:41 +0530

    Add project files.

---
+++ MovieApp.Data/DataConnection/MovieDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Entity;
+
+namespace MovieApp.Data.DataConnection
+{
+    public class MovieDBContext:DbContext //dbcontext: import ms.entityframework
+    {
+        public MovieDBContext(DbContextOptions<MovieDBContext> options): base(options)
+        {
+
+        }
+        public DbSet<UserModel> userModel { get; set; }  //DbSet is given to create table
+
+        public DbSet<MovieModel> movieModel { get; set; }
+
+        public DbSet<TheatreModel> theatreModel { get; set; }
+        
+        public DbSet<MovieShowtimeModel> movieShowtimeModel { get; set; }
+
+        public DbSet<BookingModel> bookingModel { get; set; }
+
+
+    }
+}
---
+++ MovieApp.Data/Migrations/20220603125722_addingdb.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieApp.Data.Migrations
+{
+    public partial class addingdb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "bookingModel",
+                columns: table => new
+                {
+                    BookingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false),
+                    TheatreId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Time = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Seats = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bookingModel", x => x.BookingId);
+                    table.ForeignKey(
+                        name: "FK_bookingModel_movieModel_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "movieModel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_bookingModel_theatreModel_TheatreId",
+                        column: x => x.TheatreId,
+                        principalTable: "theatreModel",
+                        principalColumn: "TheatreId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_bookingModel_userModel_UserId",
+                        column: x => x.UserId,
+                        principalTable: "userModel",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookingModel_MovieId",
+                table: "bookingModel",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookingModel_TheatreId",
+                table: "bookingModel",
+                column: "TheatreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_bookingModel_UserId",
+                table: "bookingModel",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bookingModel");
+        }
+    }
+}
